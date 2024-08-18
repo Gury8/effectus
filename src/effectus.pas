90a820cd5078879f1338f99883bffcd60edc803f
Gury8 <64693520+Gury8@users.noreply.github.com>
2024-08-18 10:35:27 +0200

    Version 0.5.6
    - Proper handling of strings and their length in BYTE ARRAY declaration
        - String length is determined by the first element of array (0th element, f.e. str(0))
        - InputSD and InputMD procedure proper handling of strings using new internal Mad Pascal variable eff_array_buffer
    
    - Shell
        - Parameter list for local routines
        - Showing Mad Pascal errors
        - Support for Neo6502 and Commander X16 computer board system (Mad Pascal support, not tested using Effectus)
    
    - Some changes in examples repository
    
    - Minor code optimization
    
    - Effectus source code listing
        - Updated header information
        - Updated routine description headers
        - Source code refactoring and cleaning
    
    - Effectus 0.5.6 was compiled with Free Pascal 3.2.2
    
    - Included cross-compilers
        - Mad Pascal Compiler version 1.7.1 [2024/08/14] for MOS 6502 CPU
        - Mad Assembler 2.1.7 build 33 (1 Aug 24) (2024/08/01)

--- src/effectus.pas
+++ src/effectus.pas
@@ -1,72 +1,83 @@
 {
-  Program: Effectus - Action! language parser and cross-assembler to native binary code
-           for Atari 8-bit home computers
+    Program: Effectus
+        Effectus parses Action! language source code listings and generates native binary code
+        for 8-bit Atari home computers by using excellent Mad Pascal and Mad Assembler languages.
 
-  Authors : Bostjan Gorisek (Effectus)
-            Tebe (Mad Assembler, Mad Pascal)
-            zbyti, Mariusz Buk (Effectus support, new features, bug fixes and refactoring)
+    Authors:
+        Bostjan Gorisek (Gury): Effectus
+        Tomasz Biela (Tebe)   : Mad Assembler, Mad Pascal
+        zbyti and others      : Effectus support, new features, bug fixes...
 
-  Unit file  : effectus.pas
-  Description: Main project file
+    Effectus v0.5.6 is compiled with Free Pascal 3.2.2.
 
-  Effectus parses Action! language source code listings and generates native binary code
-  for 8-bit Atari home computers by using excellent Mad Pascal and Mad Assembler languages.
+    References:
+        https://gury.atari8.info/effectus/
+        https://github.com/Gury8/effectus
+        https://github.com/mariusz-buk/effectus
 
-  Effectus is compiled with Free Pascal 3.0.4.
+        https://github.com/tebe6502/Mad-Pascal
+        https://github.com/tebe6502/Mad-Assembler
 
-  References:
-    https://github.com/mariusz-buk/effectus
-    http://freeweb.siol.net/diomedes/effectus/
-    http://mads.atari8.info/
+    This program is free software: you can redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the Free Software Foundation, either version 3
+    of the License, or (at your option) any later version.
 
-  This program is free software: you can redistribute it and/or modify it under the terms of
-  the GNU General Public License as published by the Free Software Foundation, either version 3
-  of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
 
-  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with this program.
+    If not, see <http://www.gnu.org/licenses/>.
 
-  You should have received a copy of the GNU General Public License along with this program.
-  If not, see <http://www.gnu.org/licenses/>.
+    To compile Effectus source code, use the following statement from command line:
+      fpc -MDelphi -vh -O3 src\effectus.pas
+
+    Program file: effectus.pas
+    Description : Main project file
 }
 program Effectus;
 {$APPTYPE CONSOLE}
 
 uses
-  Classes, SySutils, Decl, Shell, Core;
+    Classes, SySutils, Process, StrUtils, Crt;
 
 var
-  i : Byte;
-  isTranslation : boolean = false;
+    i : byte;
+    isTranslation : boolean = false;
+
+{$I decl.inc}
+{$I lib.inc}
+{$I shell.inc}
+{$I core.inc}
 
 begin
-  CreateLists;
-  ShellProc;
-  Init;
-  Writeln('Reading Action! source file...', actionFilename);
-  ReadSource(actionFilename);
-  Writeln('Generating code...');
+    CreateLists;
+    ShellProc;
+    Init;
+    Writeln('Reading Action! source file...', actionFilename);
+    ReadSource(actionFilename);
+    Writeln('Generating code...');
 
-  GenerateCode;
+    GenerateCode;
 
-  for i := 1 to ParamCount do begin
-    if ParamStr(i) = '-t' then begin
-      isTranslation := true;
-      Writeln('Translation only...');
-      break;
+    for i:=1 to ParamCount do begin
+        if ParamStr(i) = '-t' then begin
+            isTranslation:=true;
+            Writeln('Translation only...');
+            break;
+        end;
     end;
-  end;
-  if not isTranslation then begin
-    Writeln('Compiling to native code...');
-    if Compile(FilenameSrc) then begin
-      Writeln('Compile error!');
-    end
-    else begin
-      Writeln('Compilation is successful!');
-    end;
-    if isInfo then ShellInfo;
-  end;
 
-  DestroyLists;
+    if not isTranslation then begin
+        Writeln('Compiling to native code...');
+
+        if Compile(FilenameSrc) then
+            Writeln('Compilation is successful!');
+        //else
+        //    Writeln('Compile error!');
+
+        if isInfo then ShellInfo;
+    end;
+
+    DestroyLists;
 end.
